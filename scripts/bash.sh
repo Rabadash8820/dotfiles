5fe2e34bcfb59c2f3de7d890dc84729fcf4244bb
Dan Vicarel <dvic@derploid.com>
2022-10-24 03:12:10 -0400

    Add shortcut aliases to bash install script

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
 echo "Setting up Dan's bash dotfiles..."
+
+echo "Setting shell shortcut aliases..."
+
+#TODO: Save these aliases for user
+
+alias lsa="ls --all"
+alias lsal="ls --all -l"
+alias lsl="ls -l"
+
+alias rmrf="rm --force --recursive"
+
+alias src="source ~/.bashrc"
+alias virc="vim ~/.bashrc"
+
+alias wcb="wcb --bytes"
+alias wcl="wc --lines"
+alias wcm="wcm --chars"
+alias wcm="wcw --words"
