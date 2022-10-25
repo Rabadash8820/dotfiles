8b09e16647e25fa82f2c9594b6dcb16ea8e98c86
Dan Vicarel <dvic@derploid.com>
2022-10-24 23:07:06 -0400

    Bash install script now "installs" aliases idempotently

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -2,13 +2,26 @@
 
 echo "Setting up Dan's bash dotfiles..."
 
+shellRcPath=/home/$SUDO_USER/.bashrc
+
+aliasRcComment="Auto-added aliases from Dan's dotfiles repo"
+lineCount=$(grep "$aliasRcComment" "$shellRcPath" | wc --lines)
+if [ $lineCount -gt 0 ]; then
+    echo "Shell shortcut aliases already set"
+    exit 0
+fi
+
 echo "Setting shell shortcut aliases..."
 
-#TODO: Save these aliases for user
+(cat <<EOF
+
+# $aliasRcComment
 
 alias lsa="ls --all"
 alias lsal="ls --all -l"
 alias lsl="ls -l"
+alias lslr="ls -l --recursive"
+alias lsr="ls --recursive"
 
 alias rmrf="rm --force --recursive"
 
@@ -19,3 +32,5 @@ alias wcb="wcb --bytes"
 alias wcl="wc --lines"
 alias wcm="wcm --chars"
 alias wcm="wcw --words"
+EOF
+) >> "$shellRcPath"
