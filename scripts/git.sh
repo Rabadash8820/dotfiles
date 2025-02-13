cb35473d0da9aa9574a8696d809d32b077ca65a2
Dan Vicarel <dan.vicarel@gmail.com>
2025-02-13 02:33:22 -0500

    Update git config settings
    Set user name/email/signingkey if blank (latter is just a placeholder), and set some signing-related configs

--- scripts/git.sh
+++ scripts/git.sh
@@ -7,6 +7,7 @@ HOME_DIR=$(grep HOME_DIR "$cfgFilePath" | cut -d '=' -f 2)
 REPOS_FOLDER=$(grep REPOS_FOLDER "$cfgFilePath" | cut -d '=' -f 2)
 SHELL_RC_FILE=$(grep SHELL_RC_FILE "$cfgFilePath" | cut -d '=' -f 2)
 USER_NAME=$(grep USER_NAME "$cfgFilePath" | cut -d '=' -f 2)
+USER_EMAIL=$(grep USER_EMAIL "$cfgFilePath" | cut -d '=' -f 2)
 
 echo "Setting up Dan's git dotfiles..."
 
@@ -48,11 +49,21 @@ EOF
     ) >> "$globalGitConfigPath"
 fi
 
-# Set other git configs
+# Set other git configs (for correct non-root user)
 echo "Setting other global git configs..."
-git config --global user.name "$USER_NAME"
-git config --global commit.gpgsign true
-git config --global tag.gpgsign true
+function trySetGitConfig() {
+    sudo -u $SUDO_USER git config --global "$1" > /dev/null
+    if [ $? -gt 0 ]; then
+        sudo -u $SUDO_USER git config --global --comment "$3" "$1" "$2"
+    fi
+}
+trySetGitConfig "user.name" "$USER_NAME"
+trySetGitConfig "user.email" "$USER_EMAIL"
+trySetGitConfig "user.signingkey" "TODO"
+sudo -u $SUDO_USER git config --global --comment "Require committer name/email to be set in config, not guessed by git" user.useconfigonly true
+sudo -u $SUDO_USER git config --global commit.gpgsign true
+sudo -u $SUDO_USER git config --global tag.gpgsign true
+echo "${COLOR_WARNING}Set user.signingkey in your global git config to your desired GPG key ID$COLOR_STOP"
 
 # Save git-related shell aliases
 (cat <<EOF
