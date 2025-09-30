9881ae2d31856407913de5aded2e306a84e1980a
Dan Vicarel <dan.vicarel@gmail.com>
2025-09-30 13:38:46 -0400

    Improve GPG install script - Use `pinentry-tty` for pinentry, so we don't have to keep messing with visual pinentry thru Gpg4win - Add `--keyid-format=long` to `gpgk` alias

--- scripts/gpg.sh
+++ scripts/gpg.sh
@@ -13,7 +13,7 @@ echo "Setting up GPG for a '$MACHINE_TYPE' machine..."
 gpgVersion=$(gpg --version)
 if [ -z "$gitVersion" ] ; then
     echo "Installing gpg..."
-    apt-get install gpg gnupg2 --yes
+    apt-get install gpg gnupg2 pinentry-tty --yes
 fi
 
 gpgAgentConfPath=$HOME_DIR/.gnupg/gpg-agent.conf
@@ -24,7 +24,7 @@ if [ $? = 0 ]; then
 else
     echo "Setting GPG pinentry-program to Gpg4win..."
     echo "# Use Gpg4win in host Windows OS for pin entry" >> "$gpgAgentConfPath"
-    echo "pinentry-program /mnt/c/Program Files (x86)/Gpg4win/bin/pinentry.exe" >> "$gpgAgentConfPath"
+    echo "pinentry-program \"/usr/bin/pinentry-tty\"" >> "$gpgAgentConfPath"
 fi
 
 (cat <<EOF
@@ -33,7 +33,9 @@ alias gpgc="gpg --symmetric"
 alias gpge="gpg --encrypt"
 alias gpgd="gpg --decrypt"
 alias gpgh="gpg --help"
-alias gpgk="gpg --list-keys"
+alias gpgk="gpg --list-keys --keyid-format=long"
 alias gpgs="gpg --sign"
+
+export GPG_TTY=\$\(tty\)
 EOF
 ) | "$ADD_RC_COMMANDS_SCRIPT" "$SHELL_RC_FILE" "gpg"
